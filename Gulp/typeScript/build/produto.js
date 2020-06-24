7566d7e3e5558c2afb4519b72b8f0f1f9c1b581b
Victor Marques <victormonreal98@gmail.com>
2020-06-24 13:13:12 -0300

    configs

--- Gulp/typeScript/build/produto.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var Carro = /** @class */ (function () {
-    function Carro() {
-    }
-    return Carro;
-}());
-exports.Carro = Carro;
